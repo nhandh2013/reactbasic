34b48ed08d2cf76d0602654a329aa20b0b73ec5e
Nhan Do <nhan.do@niteco.se>
2017-08-29 16:47:53 +0700

    - Add module Marked

--- src/App.js
+++ src/App.js
@@ -6,14 +6,14 @@ let cardsList = [
     {
         id: 1,
         title: "Read the Book",
-        description: "I should read the whole book",
+        description: "I should read the **whole** book",
         status: "in-progress",
         tasks: []
     },
     {
         id: 2,
         title: "Write some code",
-        description: "Code along with the samples in the book",
+        description: "Code along with the samples in the book. The complete source can be found at [github](https://github.com/pro-react)",
         status: "todo",
         tasks: [
             {
@@ -40,49 +40,3 @@ class HelloMessage extends React.Component {
     }
 }
 export default HelloMessage;
-
-// import React, { Component } from "react";
-// import "./App.css";
-// import Header  from "./component/Header";
-// import Main from "./container/Main";
-// import Footer from "./component/Footer";
-// class Hello extends Component {
-//     render() {
-//         const word = 'a';
-//         return (
-//             <div>
-//                 hello {word}
-//             </div>
-//         )
-//     }
-// }
-// class GroceryList extends Component {
-//     render() {
-//         return (
-//             <ul>
-//                 <ListItem quality="1" name="Nhan" />
-//                 <ListItem quality="3" name="abc" />
-//                 <ListItem quality="5">test props children</ListItem>
-//             </ul>
-//         )
-//     }
-// }
-// class ListItem extends Component {
-//     render() {
-//         return (
-//             <li>
-//                 {this.props.quality} x {this.props.children}
-//             </li>
-//         )
-//     }
-// }
-// const App = () =>
-//     <div className="App">
-//         <GroceryList />
-//         <Hello word="nhan"/>
-//         <Header />
-//         <Main />
-//         <Footer />
-//     </div>;
-//
-// export default App;
--- src/Card.js
+++ src/Card.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import CheckList from './CheckList';
+import marked from 'marked';
 
 class Card extends React.Component {
     constructor() {
@@ -8,19 +9,38 @@ class Card extends React.Component {
             showDetails: false
         };
     }
+    toggleDetails() {
+        this.setState({showDetails: !this.state.showDetails});
+    }
     render() {
         let cardDetails;
+        let child1 = React.createElement('li', null, 'First Text Content');
+        let child2 = React.createElement('li', null, 'Second Text Content');
+        let child3 = React.DOM.form(
+            {className: 'commentForm'},
+            React.DOM.input({type: 'text', placeholder: 'name'}),
+            React.DOM.input({type: 'text', placeholder: 'comment'}),
+            React.DOM.input({type: 'submit', value: 'Post' })
+        )
+        let divStyle = {
+            width: 200,
+            height: 30,
+            padding: 5,
+            backgroundColor: '#ee9900'
+        }
+        let parent = React.createElement('ul', {className: 'my-list'}, child1);
         if (this.state.showDetails) {
             cardDetails = (
                 <div className="card__details">
-                    {this.props.description}
+                    <span dangerouslySetInnerHTML={{__html:marked(this.props.description)}} />
                     <CheckList cardId={this.props.id} tasks={this.props.tasks} />
                 </div>
             )
         }
         return (
             <div className="card">
-                <div className="card__title" onClick={()=> this.setState({showDetails: !this.state.showDetails})}>{this.props.title}</div>
+                <div style={divStyle}>{parent}</div>
+                <div className={this.state.showDetails ? "card__title card__title--is-open" : "card__title"} onClick={this.toggleDetails.bind(this)}>{this.props.title}</div>
                 {cardDetails}
             </div>
         )
